9b2e4a7cd3880f85d418172344ec1fba52af666e
Tanzil Sarker <71015600+tangent-tz@users.noreply.github.com>
2021-07-20 04:04:05 -0700

    commit

--- Showname.py
+++ Showname.py
@@ -6,6 +6,9 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import pathlib
+from io import StringIO
+
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 options = Options()
@@ -14,8 +17,15 @@ driver = webdriver.Chrome(PATH, options=options)
 driver.get("https://www.cineplex.com/Theatre/cineplex-cinemas-marine-gateway-and-vip")
 html=driver.page_source
 soup=BeautifulSoup(html, 'lxml')
-movie_names=soup.find_all(class_='h3 theatre-movie-title margin-vertical-xs')
-movie_names=list(dict.fromkeys(movie_names))
-for movie_name in movie_names:
-     print(f"Movie Name: {movie_name.text}")
+movies=soup.find_all(class_='movie-showtimes-row row ng-scope')
+for index, movie in enumerate(movies):
+     movie_name = movie.find(class_='h3 theatre-movie-title margin-vertical-xs').text
+     print(movie_name)
+     for index2, movie2 in enumerate(movies):
+          movie_time= movie2.find(class_="showtime-grid col-xs-12").text.replace('\n','')
+          movie_time=movie_time.replace(' ','')
+          movie_time=movie_time.replace('m','m  |  ')
+          print(movie_time)
+
+
 driver.quit()
