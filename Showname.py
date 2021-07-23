fa1c700ae66b87b5424e0c98b4363a4d93f176c2
Tanzil Sarker <71015600+tangent-tz@users.noreply.github.com>
2021-07-22 21:39:14 -0700

    Successfully gets movie name and time

--- Showname.py
+++ Showname.py
@@ -1,3 +1,4 @@
+from filter import filterjunk
 from bs4 import BeautifulSoup
 import requests
 import time
@@ -7,9 +8,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pathlib
-from io import StringIO
 
-
+movienametime=[]
+movie_name=[]
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 options = Options()
 options.headless = True
@@ -19,13 +20,24 @@ html=driver.page_source
 soup=BeautifulSoup(html, 'lxml')
 movies=soup.find_all(class_='movie-showtimes-row row ng-scope')
 for index, movie in enumerate(movies):
-     movie_name = movie.find(class_='h3 theatre-movie-title margin-vertical-xs').text
-     print(movie_name)
-     for index2, movie2 in enumerate(movies):
-          movie_time= movie2.find(class_="showtime-grid col-xs-12").text.replace('\n','')
-          movie_time=movie_time.replace(' ','')
-          movie_time=movie_time.replace('m','m  |  ')
-          print(movie_time)
+     movie_name.append(movie.find(class_='h3 theatre-movie-title margin-vertical-xs').text)
+     #print(movie_name)
+for index2, movie2 in enumerate(movies):
+     movie_time= movie2.find(class_="col-xs-12 movie-showtimes-section").text.replace('\n','')
+     if 'https://mediafiles.cineplex.com/img/showtime_experience/vip19plus.png' in movie_time:
+          times=movie_time.find(id='l_1145_s_69214_c_0000000001').text
+     movie_time=movie_time.replace(' ','')
+     movie_time=movie_time.replace('0pm','0pm|')
+     movie_time=movie_time.replace('5pm','5pm|')
+     movie_time=movie_time.replace('0am','0am|')
+     movie_time=movie_time.replace('5am','5am|')
+     movienametime.append(filterjunk(movie_time,movie_name))
+
+for i in range(len(movie_name)):
+     print(movie_name[i])
+     print(movienametime[i],"\n")
+
+          
 
 
 driver.quit()
---
+++ filter.py
@@ -0,0 +1,14 @@
+
+def filtermovienames(movie_name):
+    for i in range(len(movie_name)):
+        movie_name[i]=movie_name[i].replace(' ','')
+    return movie_name
+
+def filterjunk(movie_time,movie_name):
+    filter=['pairedandDescriptiveServiceforthevisuallyim','paired(CC/DS)3DATMOSULTRAAVXReclinerSeatsFAUTEUILSINCLINABLES(CC/DS)','trailerClosedCaptionsforthehearingim','ClosedCaptionsforthehearingim','Nopassestrailer3DATMOSULTRAAVXReclinerSeatsFAUTEUILSINCLINABLES','Nopassestrailer','||','|||','trailer','Nopasses']
+    extrafilters=filtermovienames(movie_name)
+    for i in range(len(extrafilters)):
+        filter.append(extrafilters[i])
+    for i in range(len(filter)):
+        movie_time=movie_time.replace(filter[i],'')
+    return movie_time
