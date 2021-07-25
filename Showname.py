86c9855ae6c224a6714c00b369b97e4bdf9875a5
Tanzil Sarker <71015600+tangent-tz@users.noreply.github.com>
2021-07-24 22:21:46 -0700

    minor changes

--- Showname.py
+++ Showname.py
@@ -11,26 +11,21 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pathlib
 
-movienametime=[]
-movie_name=[]
-moviename=[]
-PATH = "C:\Program Files (x86)\chromedriver.exe"
-options = Options()
-options.headless = True
-driver = webdriver.Chrome(PATH, options=options)
-driver.get("https://www.cineplex.com/Theatre/cineplex-cinemas-marine-gateway-and-vip")
-html=driver.page_source
-soup=BeautifulSoup(html, 'lxml')
-movies=soup.find_all(class_='movie-showtimes-row row ng-scope')
-moviename=getMovieNames(movies,movie_name,moviename)
-movienametime=getMovieTimes(movies,movie_name, movienametime)
-
-
-for i in range(len(movie_name)):
-     print(moviename[i])
-     print(movienametime[i],"\n")
-
-          
-
-
-driver.quit()
+if __name__ == "__main__":
+     movienametime=[]
+     movie_name=[]
+     moviename=[]
+     PATH = "C:\Program Files (x86)\chromedriver.exe"
+     options = Options()
+     options.headless = True
+     driver = webdriver.Chrome(PATH, options=options)
+     driver.get("https://www.cineplex.com/Theatre/cineplex-cinemas-marine-gateway-and-vip")
+     html=driver.page_source
+     soup=BeautifulSoup(html, 'lxml')
+     movies=soup.find_all(class_='movie-showtimes-row row ng-scope')
+     moviename=getMovieNames(movies,movie_name,moviename)
+     movienametime=getMovieTimes(movies,movie_name, movienametime)
+     for i in range(len(movie_name)):
+          print(moviename[i])
+          print(movienametime[i],"\n")
+     driver.quit()
