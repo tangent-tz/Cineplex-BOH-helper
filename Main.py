393d64c9ea3527d8455a0e2bd9f3b91940c2addb
Tanzil Sarker <71015600+tangent-tz@users.noreply.github.com>
2022-02-28 02:11:30 -0800

    Switch from selenium to extract ticket sales to beautiful soup

--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
+import Development
 from Parsers import *
 from Showtype import getShowTypes
 from MovieNames import *
@@ -18,63 +19,62 @@ import pathlib
 import urllib.request
 import json
 
-#Features implmeneted so far
+# Features implmeneted so far
 #    Success fully retreives the name of all the movie running in the threatre, Check Function getMovieNames()
 #    Recovers all the type of threates (DBOX/ULTRAAVX/VIP/REGULAR/ETC) running in the threatres
 #    Recovers all the movie running time from the threates
 #    Recovers all the ticket sales so far for the movies
 
 if __name__ == "__main__":
+    movie_time = []
+    movie_name = []
+    moviename = []
+    movie_time_refined = []
 
-     movie_time=[]
-     movie_name=[]
-     moviename=[]
-     movie_time_refined=[]
+    PATH = "C:\Program Files (x86)\chromedriver.exe"
+    options = Options()
+    # options.add_argument('--no-sandbox')
+    # options.add_argument("--start-maximized")
+    # options.add_argument("--window-size=1920x1080")
+    options.add_argument('--disable-extensions')
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--proxy-bypass-list=*")
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument("--headless")
+    driver = webdriver.Chrome(PATH, options=options)
+    driver.execute_cdp_cmd("Page.setBypassCSP", {"enabled": True})
 
-     PATH = "C:\Program Files (x86)\chromedriver.exe"
-     options = Options()
-     options.add_argument('--no-sandbox')
-     options.add_argument("--start-maximized")
-     options.add_argument("--window-size=1920x1080")
-     options.add_argument('--disable-extensions')
-     options.add_argument("--disable-gpu")
-     options.add_argument("--disable-dev-shm-usage")
-     options.add_argument("--proxy-bypass-list=*")
-     options.add_argument('--ignore-certificate-errors')
-     
-     driver = webdriver.Chrome(PATH, options=options)
-     driver.execute_cdp_cmd("Page.setBypassCSP", {"enabled": True})
+    # Getting page source code
+    driver.get("https://www.cineplex.com/Theatre/cineplex-cinemas-marine-gateway-and-vip")
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'lxml')
+    # Gets Movie Names
+    moviename = getMovieNames(soup, movie_name, moviename)
 
-#Getting page source code
-     driver.get("https://www.cineplex.com/Theatre/cineplex-cinemas-marine-gateway-and-vip")
-     html=driver.page_source
-     soup=BeautifulSoup(html, 'lxml')
-#Gets Movie Names
-     moviename=getMovieNames(soup,movie_name,moviename)
+    # Gets Movie Schedule
+    movie_time = getMovieTimes(soup)
 
-#Gets Movie Schedule
-     movie_time=getMovieTimes(soup)
+    # Gets Movie Screen type
+    showtypes = getShowTypes(soup)
 
-#Gets Movie Screen type
-     showtypes=getShowTypes(soup)
-     
-#Gets number of seats sold
-     # url_seating=getMovieTicketUrl(soup)
-     # movie_time_refined=pretty_MovieTime(movie_time)
-     # ticket_sales_status=getTicketSales(soup)
-     
-     # seats=getTicketSoldStatus(url_seating, options)
-     # show_type_per_movie=getScreenTypesPerMovie(soup)
-     # movie_per_show_type=getMoviePerScreenType(movie_time_refined)
-          
-     # temp=movieSalesStatus(ticket_sales_status, seats)
-     # Guestlist=guestnumber(movie_per_show_type, temp)
-
-     # test=printer(movie_name,show_type_per_movie,showtypes,movie_time_refined, Guestlist)
-     # print(test)
-     # with open('MovieData.json', 'w') as fp:
-     #      json.dump(test, fp)
-     with open('MovieData.json', 'r') as fp:
-          data = json.load(fp)
-          print(data['0']['Name'])
-     driver.quit()
+    # Gets number of seats sold
+    url_seating = getMovieTicketUrl(soup)
+    movie_time_refined = pretty_MovieTime(movie_time)
+    ticket_sales_status = getTicketSales(soup)
+    #
+    # seats=getTicketSoldStatus(url_seating, options)
+    seats = Development.test(url_seating)
+    show_type_per_movie = getScreenTypesPerMovie(soup)
+    movie_per_show_type = getMoviePerScreenType(movie_time_refined)
+    #
+    temp = movieSalesStatus(ticket_sales_status, seats)
+    Guestlist = guestnumber(movie_per_show_type, temp)
+    #
+    test = printer(movie_name, show_type_per_movie, showtypes, movie_time_refined, Guestlist)
+    print(test)
+    with open('MovieData.json', 'w') as fp:
+         json.dump(test, fp)
+    with open('MovieData.json', 'r') as fp:
+         data = json.load(fp)
+    driver.quit()
